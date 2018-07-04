1d58e0e135303d07159d1fbde1c41c53708741fd
Kushin <kushinm11@gmail.com>
2018-07-04 16:06:17 -0700

    Added svg string clean up

--- Sandbox/annotation.js
+++ Sandbox/annotation.js
@@ -6,7 +6,9 @@ window.onload = function() {
   //var newPath
   //Storing SVG data for JSON file in smile
   var smile = smiley['svgData'];
-
+  base = new CompoundPath(smile.join());
+  base.strokeColor = 'red';
+  base.strokeWidth = 10;
   //Iterating over the length of the SVG string and storing each stroke in a different thisBinding
 
   var pathArray = new Array;
@@ -17,11 +19,22 @@ window.onload = function() {
     pathArray[i].strokeWidth = 5;
 
   };
-   
 
-  console.log(pathArray[2].exportSVG({asString: true}));
-  TestPath = pathArray[2]; //setting this.marker0 to a new variable name since there are issues otherwise
+  //Testing objects to create final json file for export
+  var testObj = new Object();
+  testObj.sub1 =[];
+  testObj.sub2 = [];
+  for (var i = 0; i<pathArray.length; i++){
+    testObj.sub1[i] = i;
+    testObj.sub2[i] = pathArray[i].exportSVG({asString: true}).substring(44, pathArray[i].exportSVG({asString: true}).indexOf('"',44));
+  }
+  console.log(JSON.parse(JSON.stringify(testObj)).sub1);
+  svgstring = pathArray[2].exportSVG({asString: true})
+  console.log(svgstring.substring(44, svgstring.indexOf('"',44)));
+  console.log(smile[2])
 
+
+  //TestPath = pathArray[2]; 
   //The commented out function below is for segment wise highlighting. You'll notice the need for
   //changing the this.marker0 variable name here, since "this" refers to something else in the function I think
   /*TestPath.onMouseEnter = function(event){
@@ -29,33 +42,59 @@ window.onload = function() {
        newPath.strokeColor = 'blue';
        newPath.strokeWidth = 8;} */  
 
+
+
   //highlight  helper functions
 
-  TestPath.alreadyClicked = false;
+  isfalse = function(element){
+    if(element == false){
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
 
-  TestPath.onClick = function(event){
-     TestPath.strokeColor = 'red';
-     TestPath.alreadyClicked = true;
+  pathArray.alreadyClicked = new Array(pathArray.length);
+  pathArray.alreadyClicked.fill(false);
+
+  //Conditional statement for final version
+ if(pathArray.alreadyClicked.some(isfalse) = true){
+
+ }
+  
+   
+ // Code for click highlight
+  pathArray[3].onClick = function(event){
+    console.log(feMouseEvent);
+     pathArray[3].strokeColor = 'red';
+     pathArray.alreadyClicked[3] = true;
   };
-
   //Code for hover highlight
-  TestPath.onMouseEnter = function(event){
-  if(TestPath.alreadyClicked == false){
-   TestPath.strokeColor= 'blue';}
+
+
+  
+  pathArray[3].onMouseEnter = function(event){
+  if(pathArray.alreadyClicked[3] == false){
+    pathArray[3].strokeColor= 'blue';}
   };
 
-  TestPath.onMouseLeave = function(event){
+  pathArray[3].onMouseLeave = function(event){
     // if(TestPath.strokeColor != 'red'){
     //   console.log(TestPath.strokeColor);
     //   TestPath.strokeColor = 'black';
     // }
-    if(TestPath.alreadyClicked == false){
-      console.log(TestPath.strokeColor);
-      TestPath.strokeColor = 'black';
+    if(pathArray.alreadyClicked[3] == false){
+      pathArray[3].strokeColor = 'black';
     }
 
-  };
-   
+   };
+ };
+
+
+
+
+
   var myPath;
 
   // drawing helper functions
@@ -101,7 +140,7 @@ window.onload = function() {
     // Whenever the window is resized, recenter the path:
     smilepath.position = view.center;
 
-  };  
+  }; 
+  
 
 
-}
