1d02d72c00956b284eaa1b56f71b21f7d7fca017
Kushin <kushinm11@gmail.com>
2018-07-16 17:11:14 -0700

    more console log

--- experiments/part_annotation.js
+++ experiments/part_annotation.js
@@ -75,7 +75,7 @@ function setRandomColor(li) {
       	pathArray[i].strokeColor = 'black';
       	//Increasing stroke width to make it clickable
       	pathArray[i].strokeWidth = 8;
-      console.log("Display is working", sketch[9]);
+      console.log("Display is working", sketch[3]);
 
   	//Click and Hover event handlers
   	_.forEach(pathArray, function(p) {
