892321183cc5249a3e6cdc79b74b353d697bccd0
Judy Fan <judithfan@gmail.com>
2018-07-31 11:39:37 -0700

    update name of collection to svg_annotation_sketchpad_basic_chairs

--- experiments/app.js
+++ experiments/app.js
@@ -86,7 +86,7 @@ function sendStim(socket, data) {
   sendPostRequest('http://localhost:6001/db/getstims', {
     json: {
       dbname: 'stimuli',
-      colname: 'svg_annotation_sketchpad_basic',
+      colname: 'svg_annotation_sketchpad_basic_chairs',
       numTrials: 1,
       gameid: data.gameID
     }
