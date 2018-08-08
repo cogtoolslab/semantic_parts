862953b3cc72866502470caf2b0b9be4f4a07356
Judy Fan <judithfan@gmail.com>
2018-08-07 19:02:52 -0700

    set up analysis pipeline for svgannotations

--- analysis/analysis_helpers.py
+++ analysis/analysis_helpers.py
@@ -1,6 +1,19 @@
 import os
 import numpy as np
 import seaborn as sns
+import pandas as pd
 
 
 ## this is where we will place analysis helper functions
+
+def convert_numeric(X,column_id):
+    ## make numeric types for aggregation
+    X[column_id] = pd.to_numeric(X[column_id])
+    return X
+
+
+def remove_nans(x):
+    nan_inds = np.isnan(x)
+    _x = x[nan_inds==False]
+    return _x
+    
