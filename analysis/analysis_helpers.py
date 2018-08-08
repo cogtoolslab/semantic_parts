f13446be5545a8c05209b493d6c0bb615829e50f
Kushin <kushinm11@gmail.com>
2018-08-07 21:52:41 -0700

    added encode/decode to analysis helpers

--- analysis/analysis_helpers.py
+++ analysis/analysis_helpers.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import seaborn as sns
 import pandas as pd
+import base64
 
 ## this is where we will place analysis helper functions
 
@@ -16,3 +17,20 @@ def remove_nans(x):
     _x = x[nan_inds==False]
     return _x
     
+
+def encode(key, clear):
+    enc = []
+    for i in range(len(clear)):
+        key_c = key[i % len(key)]
+        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
+        enc.append(enc_c)
+    return base64.urlsafe_b64encode("".join(enc))
+
+def decode(key, enc):
+    dec = []
+    enc = base64.urlsafe_b64decode(enc)
+    for i in range(len(enc)):
+        key_c = key[i % len(key)]
+        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+        dec.append(dec_c)
+    return "".join(dec)
