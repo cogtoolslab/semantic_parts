1677af9720810e2de40f83cd9c9a9126644800c1
Kushin <kushinm11@gmail.com>
2018-11-03 20:33:04 -0400

    changed db name in app.js

--- experiments/app.js
+++ experiments/app.js
@@ -86,7 +86,7 @@ function sendStim(socket, data) {
   sendPostRequest('http://localhost:6001/db/getstims', {
     json: {
       dbname: 'stimuli',
-      colname: 'svg_annotation_sketchpad_basic_chairs4',
+      colname: 'svg_annotation_sketchpad_basic_allcats_dev',
       numTrials: 1,
       gameid: data.gameID
     }
