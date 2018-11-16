6fc1f5537cc3246f2b4082131ec0b27b523bf293
Judy Fan <judithfan@gmail.com>
2018-11-15 23:55:09 -0800

    pull from actual stim collection

--- experiments/app.js
+++ experiments/app.js
@@ -86,7 +86,7 @@ function sendStim(socket, data) {
   sendPostRequest('http://localhost:6001/db/getstims', {
     json: {
       dbname: 'stimuli',
-      colname: 'svg_annotation_sketchpad_basic_allcats_dev',
+      colname: 'svg_annotation_sketchpad_basic_allcats',
       numTrials: 1,
       gameid: data.gameID
     }
